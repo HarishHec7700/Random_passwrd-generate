5a58b0c8504e1dd7ef2a0e4ad9bcca88596de576
HarishHec7700 <harish.hariharan7@gmail.com>
2022-10-28 14:10:53 +0530

    Random Password Generator

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let allchar="12jklmn789!@%0ab*(c3456defghiopt#$uvwxyzABC^&)DEFGHIJKLMNOPQqrsRSTUVWXYZ",
 btnEle=document.getElementById("gen"),passwrd="";
 inpCharEle=document.getElementById("noChar"),
+copyEle=document.getElementById("copy"),
 resPass=document.getElementById("passRes"),result=document.getElementById("result"),
 ranGen=(numChar)=>{
     let ran=Math.floor(Math.random()*numChar);
@@ -12,7 +13,6 @@ ranGen=(numChar)=>{
         
 },
 ranPassGen=()=>{
-    console.log("hi");
     resetRes();
     let noChar=inpCharEle.value;
     for(i=0;i<noChar;i++){
@@ -21,9 +21,20 @@ ranPassGen=()=>{
     }
     console.log(passwrd);
     result.style.display="block";
-    resPass.innerHTML="The Generated Password is "+passwrd
+    resPass.innerHTML="The Generated Password is ";
+    copy=document.createElement("span");
+    copy.setAttribute("id","copy-text");
+    resPass.appendChild(copy);
+    copy.innerHTML=passwrd;
     inpCharEle.value=""
 };
 
+copyEle.addEventListener("click",()=>{
+    copyText=document.getElementById("copy-text").innerHTML;
+    navigator.clipboard.writeText(copyText).then(()=>{
+        alert("Password has been copied");
+    })
+
+})
 btnEle.addEventListener("click",ranPassGen);
 console.log(btnEle);
